a76d0ef286f82ec9afe33192a83e5e8cb1676993
linhtran <ltran1@brynmawr.edu>
2021-06-23 23:38:50 +0700

    23-06

--- Medium/src/KClosestPointsToOrigin.java
+++ Medium/src/KClosestPointsToOrigin.java
@@ -15,33 +15,34 @@ public class KClosestPointsToOrigin {
     Memory Usage: 115.3 MB, less than 5.25% of Java online submissions for K Closest Points to Origin.
  */
 
-    // First solution
-//    public int[][] kClosest(int[][] points, int k) {
-//        double[][] dist = new double[points.length][2];
-//        int[][] result = new int[k][2];
-//
-//        for (int i = 0; i< points.length; i++) {
-//            dist[i][0] =  Math.sqrt(points[i][0]*points[i][0] +points[i][1]*points[i][1]);
-//            dist[i][1] = (double) i;
-//        }
-//
-//        if (dist.length > 0) {
-//            Arrays.sort(dist, Comparator.comparingDouble(o -> o[0]));
-//
-//            int j=0;
-//            while (k>0){
-//                int index = (int) dist[j][1];
-//                result[j][0] = points[index][0];
-//                result[j++][1] = points[index][1];
-//                k--;
-//            }
-//        } else {
-//            return null;
-//        }
-//        return result;
-//    }
+    /** First solution
+    public int[][] kClosest(int[][] points, int k) {
+        double[][] dist = new double[points.length][2];
+        int[][] result = new int[k][2];
 
-    // Second solution: Idea credit goes to user3600W
+        for (int i = 0; i< points.length; i++) {
+            dist[i][0] =  Math.sqrt(points[i][0]*points[i][0] +points[i][1]*points[i][1]);
+            dist[i][1] = (double) i;
+        }
+
+        if (dist.length > 0) {
+            Arrays.sort(dist, Comparator.comparingDouble(o -> o[0]));
+
+            int j=0;
+            while (k>0){
+                int index = (int) dist[j][1];
+                result[j][0] = points[index][0];
+                result[j++][1] = points[index][1];
+                k--;
+            }
+        } else {
+            return null;
+        }
+        return result;
+    }
+     */
+
+    /** Second solution: Idea credit goes to user3600W */
     public int[][] kClosest(int[][] points, int k) {
         int low =0;
         int high = points.length-1;
