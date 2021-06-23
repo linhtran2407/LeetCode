122dc627e50f2a76089a06a3c034faecd905b571
linhtran <ltran1@brynmawr.edu>
2021-06-23 23:30:46 +0700

    23-06

--- Easy/src/TwoSum.java
+++ Easy/src/TwoSum.java
@@ -2,10 +2,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class TwoSum {
-    /*
+/*
     Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
     You may assume that each input would have exactly one solution, and you may not use the same element twice.
     You can return the answer in any order.
+
     Example 1:
     Input: nums = [2,7,11,15], target = 9
     Output: [0,1]
@@ -18,7 +19,7 @@ public class TwoSum {
     Example 3:
     Input: nums = [3,3], target = 6
     Output: [0,1]
-     */
+ */
 
     public static void main(String[] args){
         System.out.println(twoSum(new int[]{2,7,11,15}, 9));
