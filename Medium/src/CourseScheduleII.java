11dfbae635e175517a187ded8ce0a63cd061d673
linhtran <ltran1@brynmawr.edu>
2021-06-29 17:20:24 +0700

    29-06

--- Medium/src/CourseScheduleII.java
+++ Medium/src/CourseScheduleII.java
@@ -1,2 +1,10 @@
 public class CourseScheduleII {
+    /*
+    @author: Linh Tran
+    @version: Jun 29, 2021
+
+    Runtime and usage info of the solution:
+    Runtime: 5 ms, faster than 69.17% of Java online submissions for Course Schedule II.
+    Memory Usage: 40.1 MB, less than 65.17% of Java online submissions for Course Schedule II.
+*/
 }
