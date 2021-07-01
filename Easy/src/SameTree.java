5c294d95d25c4f1ae12abb7f437340309d2f0567
linhtran <ltran1@brynmawr.edu>
2021-07-01 16:50:13 +0700

    01-07

---
+++ Easy/src/SameTree.java
@@ -0,0 +1,35 @@
+public class SameTree {
+
+     //Definition for a binary tree node.
+      public class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode() {}
+          TreeNode(int val) { this.val = val; }
+          TreeNode(int val, TreeNode left, TreeNode right) {
+              this.val = val;
+              this.left = left;
+              this.right = right;
+          }
+     }
+
+    class Solution {
+        public boolean isSameTree(TreeNode p, TreeNode q) {
+            // DFS: O(N)
+            if (p == null && q == null){
+                return true;
+            }
+
+            if ((p == null && q !=null) || (q == null && p!= null)){
+                return false;
+            }
+
+            if (p!=null && q!=null){
+                return (p.val == q.val) && isSameTree(p.right, q.right) && isSameTree(p.left, q.left);
+            }
+
+            return true;
+        }
+    }
+}
