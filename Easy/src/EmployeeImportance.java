aa9dd9a4c4d6260207cd87ecf9445f4bc59f08b6
linhtran <ltran1@brynmawr.edu>
2021-07-01 16:12:58 +0700

    01-07

--- Easy/src/EmployeeImportance.java
+++ Easy/src/EmployeeImportance.java
@@ -22,7 +22,7 @@ public class EmployeeImportance {
     };
 
     class Solution {
-        public int getImportance(List<Employee> employees, int id) {
+        public int getImportance_BFS(List<Employee> employees, int id) {
             // BFS: traverse through the root's children
             // find all nodes' importance scores and sum up
             // SC: O(N) with N being the number of employees
@@ -51,5 +51,33 @@ public class EmployeeImportance {
 
             return sum;
         }
+    }
+
+    public int getImportance_DFS(List<Employee> employees, int id) {
+        // DFS:
+        // SC: O(M) with M being the number of total employee
+        // TC: O(N+1) with N being the number of the employee's subordinates
+
+        // key: id, val: Employee
+        HashMap<Integer, Employee> map = new HashMap<>();
+        // build the map
+        for (Employee emp: employees){
+            map.put(emp.id, emp);
+        }
+
+        return calcImportance(map, id);
+    }
+
+    private int calcImportance(HashMap<Integer, Employee> map, int id){
+        // base case
+        if (map.get(id).subordinates.size() == 0){
+            return map.get(id).importance;
+        }
+
+        for (int subId: map.get(id).subordinates){
+            map.get(id).importance += calcImportance(map, subId);
+        }
+
+        return map.get(id).importance;
     }
 }
