f2b604c8d9341fc557f69a1e6143922a8ee68f9c
linhtran <ltran1@brynmawr.edu>
2021-07-03 18:16:06 +0700

    23-06

--- Easy/src/MaximumDepthOfBinaryTree.java
+++ Easy/src/MaximumDepthOfBinaryTree.java
@@ -3,15 +3,27 @@ public class MaximumDepthOfBinaryTree {
     // Definition for a binary tree node.
     public class TreeNode {
         int val;
-        MergeTwoBinaryTrees.TreeNode left;
-        MergeTwoBinaryTrees.TreeNode right;
+        TreeNode left;
+        TreeNode right;
         TreeNode() {}
         TreeNode(int val) { this.val = val; }
-        TreeNode(int val, MergeTwoBinaryTrees.TreeNode left, MergeTwoBinaryTrees.TreeNode right) {
+        TreeNode(int val, TreeNode left, TreeNode right) {
             this.val = val;
             this.left = left;
             this.right = right;
         }
+    }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null){
+            return 0;
+        }
+
+        if (root.left == null && root.right == null){
+            return 1;
+        }
+
+        return Math.max(maxDepth(root.left)+1, maxDepth(root.right)+1);
     }
 
 }
