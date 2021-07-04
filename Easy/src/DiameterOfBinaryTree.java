243eaf1dd9142a6b61e1784374bbaf8184eaa1fc
linhtran <ltran1@brynmawr.edu>
2021-07-04 13:39:34 +0700

    04-07

--- Easy/src/DiameterOfBinaryTree.java
+++ Easy/src/DiameterOfBinaryTree.java
@@ -4,11 +4,11 @@ public class DiameterOfBinaryTree {
     // Definition for a binary tree node.
     public class TreeNode {
         int val;
-        MaximumDepthOfBinaryTree.TreeNode left;
-        MaximumDepthOfBinaryTree.TreeNode right;
+        TreeNode left;
+        TreeNode right;
         TreeNode() {}
         TreeNode(int val) { this.val = val; }
-        TreeNode(int val, MaximumDepthOfBinaryTree.TreeNode left, MaximumDepthOfBinaryTree.TreeNode right) {
+        TreeNode(int val, TreeNode left, TreeNode right) {
             this.val = val;
             this.left = left;
             this.right = right;
