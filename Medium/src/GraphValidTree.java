aacb30c8f7716ba8e1a36bac382ce940a7089b50
linhtran <ltran1@brynmawr.edu>
2021-07-04 15:50:21 +0700

    04-07

--- Medium/src/GraphValidTree.java
+++ Medium/src/GraphValidTree.java
@@ -1,5 +1,12 @@
 import java.util.*;
 
+/**
+ * Problem:
+ * You have a graph of n nodes labeled from 0 to n - 1. You are given an integer n and a list of edges
+ * where edges[i] = [ai, bi] indicates that there is an undirected edge between nodes ai and bi in the graph.
+ *
+ * Return true if the edges of the given graph make up a valid tree, and false otherwise.
+ */
 public class GraphValidTree {
 
     /*
