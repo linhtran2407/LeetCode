0b93f8f3662c9e3ba9a5ed19242ab0a622483af6
linhtran <ltran1@brynmawr.edu>
2021-07-05 17:49:47 +0700

    05-07

--- Easy/src/SubtreeOfAnotherTree.java
+++ Easy/src/SubtreeOfAnotherTree.java
@@ -1,4 +1,12 @@
 public class SubtreeOfAnotherTree {
+    /*
+    @author: Linh Tran
+    @version: July 05, 2021
+
+    Runtime and usage info of the solution:
+    Runtime: 3 ms, faster than 94.89% of Java online submissions for Subtree of Another Tree.
+    Memory Usage: 39 MB, less than 67.11% of Java online submissions for Subtree of Another Tree.
+    */
 
      // Definition for a binary tree node.
      public class TreeNode {
@@ -15,6 +23,7 @@ public class SubtreeOfAnotherTree {
      }
 
     class Solution {
+         // TC: O(M*N) with M and N being the number of nodes in tree and subtree
         public boolean isSubtree(TreeNode root, TreeNode subRoot) {
             // base case
             if (subRoot == null){return true;} // null is the subtree of any tree
