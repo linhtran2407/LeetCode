35b2042bd726b22d93da3a67326c464b25fadc38
linhtran <ltran1@brynmawr.edu>
2021-07-05 14:08:51 +0700

    05-07

---
+++ Easy/src/InvertTree.java
@@ -0,0 +1,32 @@
+public class InvertTree {
+
+    // Definition for a binary tree node.
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    class Solution {
+        public TreeNode invertTree(TreeNode root) {
+            if (root == null){
+                return null;
+            }
+
+            TreeNode cloneRight = invertTree(root.right);
+            TreeNode cloneLeft = invertTree(root.left);
+
+            root.right = cloneLeft;
+            root.left = cloneRight;
+
+            return root;
+        }
+    }
+}
