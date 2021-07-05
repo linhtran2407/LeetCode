1c0691b99627e66e0c5385c0d5adf7a7277817be
linhtran <ltran1@brynmawr.edu>
2021-07-06 00:05:33 +0700

    05-07

--- Easy/src/SameTree.java
+++ Easy/src/SameTree.java
@@ -24,19 +24,11 @@ Memory Usage: 36.3 MB, less than 74.07% of Java online submissions for Same Tree
     class Solution {
         public boolean isSameTree(TreeNode p, TreeNode q) {
             // DFS: O(N)
-            if (p == null && q == null){
-                return true;
-            }
+            if (p == null && q == null) { return true; }
 
-            if ((p == null && q !=null) || (q == null && p!= null)){
-                return false;
-            }
+            if (p == null || q == null ){ return false; }
 
-            if (p!=null && q!=null){
-                return (p.val == q.val) && isSameTree(p.right, q.right) && isSameTree(p.left, q.left);
-            }
-
-            return true;
+            return (p.val == q.val) && isSameTree(p.right, q.right) && isSameTree(p.left, q.left);
         }
     }
 }
