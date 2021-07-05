0786cc08eae1c06ca1eea2d57d09f1f2b5472fff
linhtran <ltran1@brynmawr.edu>
2021-07-05 14:09:50 +0700

    05-07

--- Easy/src/InvertTree.java
+++ Easy/src/InvertTree.java
@@ -1,4 +1,12 @@
 public class InvertTree {
+    /*
+        @author: Linh Tran
+        @version: July 05, 2021
+
+        Runtime and usage info of the solution:
+        Runtime: 0 ms, faster than 100.00% of Java online submissions for Invert Binary Tree.
+        Memory Usage: 36.4 MB, less than 67.35% of Java online submissions for Invert Binary Tree.
+    */
 
     // Definition for a binary tree node.
     public class TreeNode {
@@ -14,7 +22,7 @@ public class InvertTree {
         }
     }
 
-    class Solution {
+    class Solution { // iterative
         public TreeNode invertTree(TreeNode root) {
             if (root == null){
                 return null;
