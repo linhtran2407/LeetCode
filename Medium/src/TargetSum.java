a42216c5fb401007a3c5dbddf217f7471f71870c
linhtran <ltran1@brynmawr.edu>
2021-07-07 11:45:56 +0700

    07-07

---
+++ Medium/src/TargetSum.java
@@ -0,0 +1,23 @@
+public class TargetSum {
+
+
+    // iterative
+    class Solution {
+        int count = 0;
+        public int findTargetSumWays(int[] nums, int target) {
+            dfs(nums,0,0,target);
+            return count;
+        }
+
+        private void dfs(int[] nums, int index, int sum, int target){
+            if (index == nums.length){
+                if (sum == target){
+                    count++;
+                }
+            } else {
+                dfs(nums, index+1, sum + nums[index], target);
+                dfs(nums, index+1, sum - nums[index], target);
+            }
+        }
+    }
+}
