bdad56c4279f749b9cf1236a8d739496c8c80e7d
linhtran <ltran1@brynmawr.edu>
2021-07-08 16:13:55 +0700

    08-07

--- Medium/src/PacificAtlanticWaterFlow.java
+++ Medium/src/PacificAtlanticWaterFlow.java
@@ -10,6 +10,10 @@ import java.util.Queue;
 Runtime and usage info of DFS solution:
 Runtime: 4 ms, faster than 81.29% of Java online submissions for Pacific Atlantic Water Flow.
 Memory Usage: 40.5 MB, less than 42.50% of Java online submissions for Pacific Atlantic Water Flow.
+
+Runtime and usage info of BFS solution:
+Runtime: 14 ms, faster than 22.78% of Java online submissions for Pacific Atlantic Water Flow.
+Memory Usage: 40 MB, less than 87.65% of Java online submissions for Pacific Atlantic Water Flow.
 */
 
 public class PacificAtlanticWaterFlow {
