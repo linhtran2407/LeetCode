06bf1e64d215a338ed397cc2363d33b0727a72b7
linhtran <ltran1@brynmawr.edu>
2021-07-27 17:22:28 +0700

    27-7

--- Medium/src/PathSumIII.java
+++ Medium/src/PathSumIII.java
@@ -2,15 +2,14 @@ import java.util.Map;
 
 public class PathSumIII {
 
-    /**
      // Definition for a binary tree node.
-     * public class TreeNode {
-     *     int val;
-     *     TreeNode left;
-     *     TreeNode right;
-     *     TreeNode() {}
-     *     TreeNode(int val) { this.val = val; }
-     *     TreeNode(int val, TreeNode left, TreeNode right) {
+     public class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode() {}
+          TreeNode(int val) { this.val = val; }
+          TreeNode(int val, TreeNode left, TreeNode right) {
      *         this.val = val;
      *         this.left = left;
      *         this.right = right;
