efd84970322e48fae6a09a503b17d0a6b54e2f39
linhtran <ltran1@brynmawr.edu>
2021-07-27 17:22:43 +0700

    27-7

--- Medium/src/PathSumIII.java
+++ Medium/src/PathSumIII.java
@@ -10,12 +10,12 @@ public class PathSumIII {
           TreeNode() {}
           TreeNode(int val) { this.val = val; }
           TreeNode(int val, TreeNode left, TreeNode right) {
-     *         this.val = val;
-     *         this.left = left;
-     *         this.right = right;
-     *     }
-     * }
-     */
+              this.val = val;
+              this.left = left;
+              this.right = right;
+          }
+     }
+
     class Solution {
         int count = 0;
         int target;
