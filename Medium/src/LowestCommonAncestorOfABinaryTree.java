56fc0ed186af57b8d723024e6beb0f15f173a2b1
linhtran <ltran1@brynmawr.edu>
2021-07-29 18:27:06 +0700

    29/7

--- Medium/src/LowestCommonAncestorOfABinaryTree.java
+++ Medium/src/LowestCommonAncestorOfABinaryTree.java
@@ -3,7 +3,7 @@ public class LowestCommonAncestorOfABinaryTree {
         @author: Linh Tran
         @version: July 29, 2021
 
-        Runtime and usage info of the solution:
+        Runtime and usage info of first solution:
         Runtime: 7 ms, faster than 30.95% of Java online submissions for Lowest Common Ancestor of a Binary Tree.
         Memory Usage: 45.4 MB, less than 5.05% of Java online submissions for Lowest Common Ancestor of a Binary Tree.
     */
@@ -17,7 +17,7 @@ public class LowestCommonAncestorOfABinaryTree {
           TreeNode(int x) { val = x; }
       }
 
-    class Solution {
+    class Solution1 {
         TreeNode ans;
         public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
             dfs(root, p ,q);
@@ -39,6 +39,39 @@ public class LowestCommonAncestorOfABinaryTree {
 
             // any one of the flags is true is enough
             return (mid+left+right)>0;
+        }
+    }
+
+    class Solution2 {
+        Map<TreeNode, TreeNode> parentMap;
+        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+            parentMap = new HashMap<>();
+            findParents(root, null, p, q);
+
+            // add all p's ancestors to the set
+            Set<TreeNode> pAncestors = new HashSet<>();
+            while(p != null){
+                pAncestors.add(p);
+                p = parentMap.get(p);
+            }
+
+            // traverse q's parent(s) until LCA found
+            while(!pAncestors.contains(q)){
+                q = parentMap.get(q);
+            }
+
+            return q;
+        }
+
+        private void findParents(TreeNode node, TreeNode parent, TreeNode p, TreeNode q){
+            if (node == null){return;}
+
+            // stop dfs when found both p's and q's parents
+            if (parentMap.containsKey(p) && parentMap.containsKey(q)){return;}
+
+            parentMap.put(node, parent);
+            findParents(node.left, node, p, q);
+            findParents(node.right, node, p, q);
         }
     }
 }
