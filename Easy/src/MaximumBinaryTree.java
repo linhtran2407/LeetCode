97e8f5774277a24ecd10f77f725f495cf5fb4fe7
linhtran <ltran1@brynmawr.edu>
2021-07-30 13:51:34 +0700

    30/7

--- Easy/src/MaximumBinaryTree.java
+++ Easy/src/MaximumBinaryTree.java
@@ -1,20 +1,20 @@
 public class MaximumBinaryTree {
 
-    /**
-     * Definition for a binary tree node.
-     * public class TreeNode {
-     *     int val;
-     *     TreeNode left;
-     *     TreeNode right;
-     *     TreeNode() {}
-     *     TreeNode(int val) { this.val = val; }
-     *     TreeNode(int val, TreeNode left, TreeNode right) {
-     *         this.val = val;
-     *         this.left = left;
-     *         this.right = right;
-     *     }
-     * }
-     */
+
+     // Definition for a binary tree node.
+     public class TreeNode {
+         int val;
+         TreeNode left;
+         TreeNode right;
+         TreeNode() {}
+         TreeNode(int val) { this.val = val; }
+         TreeNode(int val, TreeNode left, TreeNode right) {
+             this.val = val;
+             this.left = left;
+             this.right = right;
+         }
+     }
+
     class Solution {
         public TreeNode constructMaximumBinaryTree(int[] nums) {
             if (nums.length == 0){return null;}
