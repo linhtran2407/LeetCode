08acc5a9a0dc73c0ce90abaf7791f9126eb4f726
linhtran <ltran1@brynmawr.edu>
2021-07-31 16:25:33 +0700

    31/7

---
+++ Medium/src/MaximumWidthBinaryTree.java
@@ -0,0 +1,17 @@
+public class MaximumWidthBinaryTree {
+
+
+     // Definition for a binary tree node.
+     public class TreeNode {
+         int val;
+         TreeNode left;
+         TreeNode right;
+         TreeNode(int x) { val = x; }
+     }
+
+    class Solution {
+        public int widthOfBinaryTree(TreeNode root) {
+
+        }
+    }
+}
