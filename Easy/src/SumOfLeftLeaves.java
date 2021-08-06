f637756d1b832e3aecd2f3a5ebd78dabdce1accd
linhtran <ltran1@brynmawr.edu>
2021-08-06 18:01:27 +0700

    06/08

--- Easy/src/SumOfLeftLeaves.java
+++ Easy/src/SumOfLeftLeaves.java
@@ -1,12 +1,12 @@
 public class SumOfLeftLeaves {
 
-    /**
-     * Definition for a binary tree node.
-     * public class TreeNode {
-     *     int val;
-     *     TreeNode left;
-     *     TreeNode right;
-     *     TreeNode() {}
+
+     // Definition for a binary tree node.
+     public class TreeNode {
+         int val;
+         TreeNode left;
+         TreeNode right;
+         TreeNode() {}
      *     TreeNode(int val) { this.val = val; }
      *     TreeNode(int val, TreeNode left, TreeNode right) {
      *         this.val = val;
