17986c99eab2f0dd5ea6791e38a5583dff4147a7
linhtran <ltran1@brynmawr.edu>
2021-08-06 17:17:15 +0700

    06/08

---
+++ Medium/src/SplitListToParts.java
@@ -0,0 +1,74 @@
+public class SplitListToParts {
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode() {}
+     *     ListNode(int val) { this.val = val; }
+     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        public ListNode[] splitListToParts(ListNode head, int k) {
+            // brute force:
+            // initialize the result as an array of k sub-linked list
+            // go through the array once, for each linked list, put a place holder (new Linked List)
+            // to indicate that how many listnodes should be in each sub-linked list
+            // go through the array twice, put the nodes into each element (sub ll) of the output
+            // according to the indicated size above
+            // TC: O(2N) -> O(N)
+            // SC: O(N+k) -> O(N)
+
+            ListNode[] res = new ListNode[k];
+            if (head == null){
+                return res;
+            }
+
+            // indicate size of each sub ll
+            int[] sizes = new int[k];
+            ListNode curr = head;
+            int index = 0;
+            while(curr != null){
+                // increment the size of subLL at each element of the output
+                sizes[index]++;
+
+                // update
+                curr = curr.next;
+                index = (index+1) % k; // to circulate
+            }
+
+            // reset current node and index
+            curr = head;
+            index = 0;
+
+            // put the nodes into each sub LL based on the size indicated as the value
+            // of each sub ll's head
+            while(curr != null){
+                int size = sizes[index]; // size of the current sub ll
+
+                ListNode pointer = null; // used to traverse through sub ll
+                // build each sub ll
+                for (int i=0; i<size; i++){
+                    // set node to be the head if there isn't a head of sub ll yet
+                    // otherwise connect with the previous node in the sub ll
+                    if (res[index] == null){
+                        res[index] = new ListNode(curr.val);
+                        pointer = res[index];
+                    } else {
+                        ListNode nextNode = new ListNode(curr.val);
+                        pointer.next = nextNode;
+                        pointer = nextNode;
+                    }
+
+                    curr = curr.next;
+                }
+
+                index++;
+            }
+
+            return res;
+        }
+    }
+}
