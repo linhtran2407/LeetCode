5ff7fd425d8df35c4fdc4790c20995bd1ab2e792
linhtran <ltran1@brynmawr.edu>
2021-08-06 18:01:43 +0700

    06/08

--- Easy/src/SumOfLeftLeaves.java
+++ Easy/src/SumOfLeftLeaves.java
@@ -7,11 +7,11 @@ public class SumOfLeftLeaves {
          TreeNode left;
          TreeNode right;
          TreeNode() {}
-     *     TreeNode(int val) { this.val = val; }
-     *     TreeNode(int val, TreeNode left, TreeNode right) {
-     *         this.val = val;
-     *         this.left = left;
-     *         this.right = right;
+         TreeNode(int val) { this.val = val; }
+         TreeNode(int val, TreeNode left, TreeNode right) {
+             this.val = val;
+             this.left = left;
+             this.right = right;
      *     }
      * }
      */
