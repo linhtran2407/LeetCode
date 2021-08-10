919b42a1337f964249d632b4be2445d8f0df4011
linhtran <ltran1@brynmawr.edu>
2021-08-10 23:43:01 +0700

    07/08

--- Medium/src/SwappingNodesInALinkedList.java
+++ Medium/src/SwappingNodesInALinkedList.java
@@ -7,9 +7,9 @@ public class SwappingNodesInALinkedList {
          ListNode next;
          ListNode() {}
          ListNode(int val) { this.val = val; }
-     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-     * }
-     */
+         ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     }
+
     class Solution {
         public ListNode swapNodes(ListNode head, int k) {
             // idea:
