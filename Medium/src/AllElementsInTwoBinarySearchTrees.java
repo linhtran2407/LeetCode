2ee5673b8bfc67b4562b6121feeefe64eaacb43e
linhtran <ltran1@brynmawr.edu>
2021-08-10 23:44:55 +0700

    07/08

--- Medium/src/AllElementsInTwoBinarySearchTrees.java
+++ Medium/src/AllElementsInTwoBinarySearchTrees.java
@@ -9,10 +9,10 @@ public class AllElementsInTwoBinarySearchTrees {
          TreeNode() {}
          TreeNode(int val) { this.val = val; }
          TreeNode(int val, TreeNode left, TreeNode right) {
-     *         this.val = val;
-     *         this.left = left;
-     *         this.right = right;
-     *     }
+             this.val = val;
+             this.left = left;
+             this.right = right;
+         }
      * }
      */
     class Solution {
