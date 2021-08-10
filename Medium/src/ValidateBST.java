c709ba29cb1915142a6fd0e235fa5c647cbc80eb
linhtran <ltran1@brynmawr.edu>
2021-08-10 23:36:02 +0700

    07/08

--- Medium/src/ValidateBST.java
+++ Medium/src/ValidateBST.java
@@ -5,10 +5,10 @@ public class ValidateBST {
      // Definition for a binary tree node.
      public class TreeNode {
          int val;
-     *     TreeNode left;
-     *     TreeNode right;
-     *     TreeNode() {}
-     *     TreeNode(int val) { this.val = val; }
+         TreeNode left;
+         TreeNode right;
+         TreeNode() {}
+         TreeNode(int val) { this.val = val; }
      *     TreeNode(int val, TreeNode left, TreeNode right) {
      *         this.val = val;
      *         this.left = left;
