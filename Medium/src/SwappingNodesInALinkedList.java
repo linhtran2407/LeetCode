5df124fac60e0d965bb97183e4c4566028c46c66
linhtran <ltran1@brynmawr.edu>
2021-08-10 23:42:41 +0700

    07/08

--- Medium/src/SwappingNodesInALinkedList.java
+++ Medium/src/SwappingNodesInALinkedList.java
@@ -1,9 +1,9 @@
 public class SwappingNodesInALinkedList {
 
 
-     * Definition for singly-linked list.
-     * public class ListNode {
-     *     int val;
+     // Definition for singly-linked list.
+     public class ListNode {
+         int val;
      *     ListNode next;
      *     ListNode() {}
      *     ListNode(int val) { this.val = val; }
