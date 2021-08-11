02b10183ca6f83ba7d7d1aa0337415263c68aa8d
linhtran <ltran1@brynmawr.edu>
2021-08-11 14:09:56 +0700

    11-8

--- Easy/src/MinimumDistanceBetweenBSTNodes.java
+++ Easy/src/MinimumDistanceBetweenBSTNodes.java
@@ -8,7 +8,7 @@ public class MinimumDistanceBetweenBSTNodes {
     Runtime: 0 ms, faster than 100.00% of Java online submissions for Minimum Distance Between BST Nodes.
     Memory Usage: 36.7 MB, less than 46.90% of Java online submissions for Minimum Distance Between BST Nodes.
 
-Runtime: 0 ms, faster than 100.00% of Java online submissions for Minimum Distance Between BST Nodes.
+    Runtime: 0 ms, faster than 100.00% of Java online submissions for Minimum Distance Between BST Nodes.
 Memory Usage: 36.7 MB, less than 33.21% of Java online submissions for Minimum Distance Between BST Nodes.
 */
      // Definition for a binary tree node.
