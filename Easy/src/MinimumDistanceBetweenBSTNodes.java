3c4a249ca8b8ff6dd985ff3728cf69576b05b4ff
linhtran <ltran1@brynmawr.edu>
2021-08-11 14:10:56 +0700

    11-8

--- Easy/src/MinimumDistanceBetweenBSTNodes.java
+++ Easy/src/MinimumDistanceBetweenBSTNodes.java
@@ -7,7 +7,8 @@ public class MinimumDistanceBetweenBSTNodes {
     Runtime and usage info of the first solution:
     Runtime: 0 ms, faster than 100.00% of Java online submissions for Minimum Distance Between BST Nodes.
     Memory Usage: 36.7 MB, less than 46.90% of Java online submissions for Minimum Distance Between BST Nodes.
-Runtime and usage info of the first solution:
+
+    Runtime and usage info of the first solution:
     Runtime: 0 ms, faster than 100.00% of Java online submissions for Minimum Distance Between BST Nodes.
     Memory Usage: 36.7 MB, less than 33.21% of Java online submissions for Minimum Distance Between BST Nodes.
 */
