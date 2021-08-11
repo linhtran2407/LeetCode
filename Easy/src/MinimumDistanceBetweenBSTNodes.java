895e939022dc5680580e64ac5be9181341ccdb12
linhtran <ltran1@brynmawr.edu>
2021-08-11 14:09:26 +0700

    11-8

--- Easy/src/MinimumDistanceBetweenBSTNodes.java
+++ Easy/src/MinimumDistanceBetweenBSTNodes.java
@@ -4,9 +4,11 @@ public class MinimumDistanceBetweenBSTNodes {
     @author: Linh Tran
     @version: July 02, 2021
 
-    Runtime and usage info of the solution:
+    Runtime and usage info of the first solution:
     Runtime: 0 ms, faster than 100.00% of Java online submissions for Minimum Distance Between BST Nodes.
     Memory Usage: 36.7 MB, less than 46.90% of Java online submissions for Minimum Distance Between BST Nodes.
+
+
 */
      // Definition for a binary tree node.
       public class TreeNode {
@@ -53,6 +55,36 @@ public class MinimumDistanceBetweenBSTNodes {
             int smallerBranch = Math.min(minDiffInBST(root.left), minDiffInBST(root.right));
 
             return Math.min(minDiff, smallerBranch);
+        }
+    }
+
+    class Solution2 {
+          Integer prev, ans;
+        public int minDiffInBST(TreeNode root) {
+            // idea: in-order traversal of BST outputs the tree in correct ascending order
+            // --> iterate the tree in-order manner, calc the difference between each node's val
+            // and its prev value
+            prev = null;
+            ans = Integer.MAX_VALUE;
+            inorder(root);
+            return ans;
+        }
+
+        private void inorder(TreeNode root){
+            if (root == null) return;
+
+            // visit the left branch first
+            inorder(root.left);
+
+            // visit root
+            if (prev != null)
+                ans = Math.min(ans, root.val - prev);
+            // set the current node's value to be prev
+            // for the next recursion
+            prev = root.val;
+
+            // visit the right branch lastly
+            inorder(root.right);
         }
     }
 }
