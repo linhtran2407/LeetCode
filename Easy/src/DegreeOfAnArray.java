951d49926edd6a9b1a125ee71286b7f11e2628dd
linhtran <ltran1@brynmawr.edu>
2021-08-28 01:10:32 -0400

    28/8

--- Easy/src/DegreeOfAnArray.java
+++ Easy/src/DegreeOfAnArray.java
@@ -1,2 +1,34 @@
 public class DegreeOfAnArray {
+
+    class Solution {
+        public int findShortestSubArray(int[] nums) {
+            Map<Integer, int[]> map = new HashMap<>();
+
+            // store first and last and frequency
+            for (int i=0; i<nums.length; ++i){
+                if (map.containsKey(nums[i])){
+                    map.put(nums[i], new int[]{map.get(nums[i])[0], i, map.get(nums[i])[2]+1});
+                } else {
+                    map.put(nums[i], new int[]{i, i, 1});
+                }
+            }
+
+            int maxOccurence = Integer.MIN_VALUE;
+            int len = Integer.MAX_VALUE;
+
+            // find max occurence and min length of subarray that has the max occurence
+            for (int item: map.keySet()){
+                int occurence = map.get(item)[2];
+
+                if (maxOccurence < occurence){
+                    len = map.get(item)[1] - map.get(item)[0]+1;
+                    maxOccurence = occurence;
+                } else if (maxOccurence == occurence){
+                    len = Math.min(len, map.get(item)[1] - map.get(item)[0]+1);
+                }
+            }
+
+            return len;
+        }
+    }
 }
