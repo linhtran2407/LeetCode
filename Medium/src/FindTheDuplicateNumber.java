552b89eb566af8c786ec5a344a96c41a2f138ed5
linhtran <ltran1@brynmawr.edu>
2021-09-05 15:01:16 -0400

    FDa

--- Medium/src/FindTheDuplicateNumber.java
+++ Medium/src/FindTheDuplicateNumber.java
@@ -1,3 +1,32 @@
 public class FindTheDuplicateNumber {
+    class Solution {
+        public int findDuplicate(int[] nums) {
+            int low = 1;
+            int high = nums.length - 1;
+            int mid = 0;
 
+
+            while (low < high){
+                mid = low + (high - low) / 2;
+                if (isDuplicated(nums, mid)) {
+                    high = mid;
+                } else {
+                    low = mid + 1;
+                }
+            }
+
+            return high;
+        }
+
+        private boolean isDuplicated (int[] nums, int checkedNum){
+            int count = 0;
+            for (int num: nums) {
+                if ( num <= checkedNum){
+                    count++;
+                }
+            }
+
+            return count > checkedNum;
+        }
+    }
 }
