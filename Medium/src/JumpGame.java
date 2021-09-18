ba5f1b61ed639034e557c673d8bfcf72b0d00f32
linhtran <ltran1@brynmawr.edu>
2021-09-18 00:11:38 -0400

    Fda

--- Medium/src/JumpGame.java
+++ Medium/src/JumpGame.java
@@ -1,5 +1,15 @@
 public class JumpGame {
 
+        /*
+    @author: Linh Tran
+    @version: Sep 18th, 2021
+
+    Runtime and usage info:
+    Runtime: 9 ms, faster than 31.60% of Java online submissions for Merge k Sorted Lists.
+    Memory Usage: 44.2 MB, less than 25.59% of Java online submissions for Merge k Sorted Lists.
+ */
+    Runtime: 1 ms, faster than 100.00% of Java online submissions for Jump Game.
+    Memory Usage: 63.7 MB, less than 25.53% of Java online submissions for Jump Game.
     class Solution {
         public boolean canJump(int[] nums) {
             // greedy
