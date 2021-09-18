eed7e2db121b544cc3a2c10ab820acb1f834f2e2
linhtran <ltran1@brynmawr.edu>
2021-09-18 00:11:05 -0400

    Fda

--- Medium/src/JumpGame.java
+++ Medium/src/JumpGame.java
@@ -6,7 +6,9 @@ public class JumpGame {
             // check from the end to see if at each index, we can reach the furthest current index
             int lastPosition = nums.length-1;
             for (int i = nums.length-1; i >= 0; i--){
+                // if from i can reach the last position
                 if (i + nums[i] >= lastPosition){
+                    // update the last position to i to cont. checking prev element
                     lastPosition = i;
                 }
             }
