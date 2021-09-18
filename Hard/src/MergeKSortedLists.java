86ae1f6f2b4d76fab20e00d4c7e95794cfad2ee0
linhtran <ltran1@brynmawr.edu>
2021-09-17 23:18:29 -0400

    Fda

--- Hard/src/MergeKSortedLists.java
+++ Hard/src/MergeKSortedLists.java
@@ -1,4 +1,6 @@
 import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class MergeKSortedLists {
 
@@ -18,6 +20,27 @@ public class MergeKSortedLists {
                 @Override
                 public int compare(ListNode o1, ListNode o2) {
                     return o1.val - o2.val;
+                }
+            }
+
+            Queue<ListNode> pq = new PriorityQueue<ListNode>(comp);
+            // initialize heap with k head nodes in k lists
+            for (ListNode l: lists){
+                if (l != null){
+                    pq.offer(l);
+                }
+            }
+
+            ListNode head = new ListNode(); // dummy head
+            ListNode curr = head;
+
+            while (!pq.isEmpty()){
+                curr.next = pq.poll();
+                curr = curr.next;
+
+                ListNode nextNodeInList = curr.next;
+                if (nextNodeInList != null){
+                    pq.offer(nextNodeInList)
                 }
             }
         }
