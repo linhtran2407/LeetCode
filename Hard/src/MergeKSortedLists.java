8a106e0032e77b56314b00a60c308ec608680d43
linhtran <ltran1@brynmawr.edu>
2021-09-17 23:16:09 -0400

    Fda

--- Hard/src/MergeKSortedLists.java
+++ Hard/src/MergeKSortedLists.java
@@ -1,3 +1,5 @@
+import java.util.Comparator;
+
 public class MergeKSortedLists {
 
      // Definition for singly-linked list.
@@ -10,6 +12,14 @@ public class MergeKSortedLists {
      }
 
     class Solution {
-
         public ListNode mergeKLists(ListNode[] lists) {
+            // implement min heap
+            Comparator<ListNode> comp = new Comparator<ListNode>() {
+                @Override
+                public int compare(ListNode o1, ListNode o2) {
+                    return o1.val - o2.val;
+                }
+            }
+        }
+     }
 }
