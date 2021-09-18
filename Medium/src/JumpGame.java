f745254d24544be5d6982fae5db8e54b6d56cc2a
linhtran <ltran1@brynmawr.edu>
2021-09-18 00:10:19 -0400

    Fda

--- Medium/src/JumpGame.java
+++ Medium/src/JumpGame.java
@@ -1,2 +1,16 @@
 public class JumpGame {
+
+    class Solution {
+        public boolean canJump(int[] nums) {
+            // greedy
+            int lastPosition = nums.length-1;
+            for (int i = nums.length-1; i >= 0; i--){
+                if (i + nums[i] >= lastPosition){
+                    lastPosition = i;
+                }
+            }
+
+            return lastPosition == 0;
+        }
+    }
 }
