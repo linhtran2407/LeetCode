d071902cd6c014a2de7d69553bc435cef96147af
linhtran <ltran1@brynmawr.edu>
2021-09-18 00:10:52 -0400

    Fda

--- Medium/src/JumpGame.java
+++ Medium/src/JumpGame.java
@@ -3,6 +3,7 @@ public class JumpGame {
     class Solution {
         public boolean canJump(int[] nums) {
             // greedy
+            // check from the end to see if at each index, we can reach the furthest current index
             int lastPosition = nums.length-1;
             for (int i = nums.length-1; i >= 0; i--){
                 if (i + nums[i] >= lastPosition){
