7e3aa8a1e7452a5be8d58c6a8f2a9f4e6dd728e8
linhtran <ltran1@brynmawr.edu>
2021-09-20 00:40:16 -0400

    fib dynamic programming

--- Hard/src/MergeKSortedLists.java
+++ Hard/src/MergeKSortedLists.java
@@ -5,25 +5,37 @@ import java.util.Queue;
 public class MergeKSortedLists {
 
     /*
-    @author: Linh Tran
-    @version: Sep 17th, 2021
+     * @author: Linh Tran
+     * 
+     * @version: Sep 17th, 2021
+     * 
+     * Runtime and usage info: Runtime: 9 ms, faster than 31.60% of Java online
+     * submissions for Merge k Sorted Lists. Memory Usage: 44.2 MB, less than 25.59%
+     * of Java online submissions for Merge k Sorted Lists.
+     */
 
-    Runtime and usage info:
-    Runtime: 9 ms, faster than 31.60% of Java online submissions for Merge k Sorted Lists.
-    Memory Usage: 44.2 MB, less than 25.59% of Java online submissions for Merge k Sorted Lists.
- */
+    // Definition for singly-linked list.
+    public class ListNode {
+        int val;
+        ListNode next;
 
-     // Definition for singly-linked list.
-     public class ListNode {
-         int val;
-         ListNode next;
-         ListNode() {}
-         ListNode(int val) { this.val = val; }
-         ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-     }
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
 
     class Solution {
         public ListNode mergeKLists(ListNode[] lists) {
+            // TC: O(N logk)
+            // SC: O(k)
             // implement min heap
             Comparator<ListNode> comp = new Comparator<ListNode>() {
                 @Override
@@ -34,8 +46,8 @@ public class MergeKSortedLists {
 
             Queue<ListNode> pq = new PriorityQueue<ListNode>(comp);
             // initialize heap with k head nodes in k lists
-            for (ListNode l: lists){
-                if (l != null){
+            for (ListNode l : lists) {
+                if (l != null) {
                     pq.offer(l);
                 }
             }
@@ -43,19 +55,18 @@ public class MergeKSortedLists {
             ListNode head = new ListNode(); // dummy head
             ListNode curr = head;
 
-            while (!pq.isEmpty()){
+            while (!pq.isEmpty()) {
                 // build result with min node in pq
                 curr.next = pq.poll();
                 curr = curr.next;
 
                 ListNode nextNodeInList = curr.next;
-                if (nextNodeInList != null){
+                if (nextNodeInList != null) {
                     pq.offer(nextNodeInList);
                 }
             }
 
-
             return head.next;
         }
-     }
+    }
 }
