dcd23427832beb1a7d4126abaf77adf4e1371d2d
linhtran <ltran1@brynmawr.edu>
2021-09-24 12:05:23 -0400

    Fd

--- Medium/src/LetterCombinationsOfAPhoneNumber.java
+++ Medium/src/LetterCombinationsOfAPhoneNumber.java
@@ -6,8 +6,8 @@ public class LetterCombinationsOfAPhoneNumber {
     }
 
     private static List<String> combinations = new ArrayList<>();
-    private Map<Character, String> map = Map.of('2', "abc", '3', "def", '4', "ghi", '5', "jkl", '6', "mno", '7', "pqrs",
-            '8', "tuv", '9', "wxyz");
+    private static Map<Character, String> map = Map.of('2', "abc", '3', "def", '4', "ghi", '5', "jkl", '6', "mno", '7',
+            "pqrs", '8', "tuv", '9', "wxyz");
 
     public static List<String> letterCombinations(String digits) {
         // back track
