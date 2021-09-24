2f3a9a0d4204b60ad0ccefeeb0fb8b93ae315d2b
linhtran <ltran1@brynmawr.edu>
2021-09-24 12:03:49 -0400

    Fd

---
+++ Medium/src/LetterCombinationsOfAPhoneNumber.java
@@ -0,0 +1,47 @@
+public class LetterCombinationsOfAPhoneNumber {
+    public static void main() {
+        System.out.println(x);
+    }
+
+    private List<String> combinations = new ArrayList<>();
+    private Map<Character, String> map = Map.of('2', "abc", '3', "def", '4', "ghi", '5', "jkl", '6', "mno", '7', "pqrs",
+            '8', "tuv", '9', "wxyz");
+
+    public List<String> letterCombinations(String digits) {
+        // back track
+        // recursion
+        // if stringbuilder == length of digits -> add to result and return
+        // pass in the index of the digit being considered
+        // loop through all char of the digit
+        // append it to a stringbuilder
+        // increase the index to move on to the next digit
+        // remove the last char in the strinbuilder for backtracking
+
+        // base case
+        if (digits.length() == 0) {
+            return combinations;
+        }
+
+        backtrack(digits, 0, new StringBuilder());
+        return combinations;
+    }
+
+    private void backtrack(String digits, int index, StringBuilder sb) {
+        // base case
+        // finish a combination
+        if (sb.length() == digits.length()) {
+            combinations.add(sb.toString());
+            return;
+        }
+
+        // all possible characters associated with the current digit
+        String characters = map.get(digits.charAt(index));
+        for (char c : characters.toCharArray()) {
+            sb.append(c);
+            backtrack(digits, index + 1, sb);
+
+            sb.deleteCharAt(sb.length() - 1);
+        }
+    }
+
+}
