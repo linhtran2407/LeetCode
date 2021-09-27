c44d69585c30e5906b35b3f94f464302b57fbe61
linhtran <ltran1@brynmawr.edu>
2021-09-27 15:46:55 -0400

    FDa

---
+++ Easy/src/PlusOne.java
@@ -0,0 +1,20 @@
+public class PlusOne {
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] == 9) {
+                digits[i] = 0;
+            } else {
+                // found the rightmost not-nine
+                digits[i]++;
+                return digits;
+            }
+        }
+
+        digits = new int[n + 1];
+        digits[0] = 1;
+
+        return digits;
+    }
+}
