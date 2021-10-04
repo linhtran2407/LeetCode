94f2ef1b21d15f5fb97c98bd5d1ebb086238d3ce
linhtran <ltran1@brynmawr.edu>
2021-10-04 00:13:42 -0400

    adfs

--- Medium/src/CombinationSum.java
+++ Medium/src/CombinationSum.java
@@ -12,7 +12,11 @@ public class CombinationSum {
     private void backtrack(int[] candidates, int remain, int start, LinkedList<Integer> comb) {
         // base case: if reach the target or exceed - i.e. cannot reach the target
         if (remain == 0) {
-            res.add(comb);
+            // create a deep copy of current combination and add to result
+            res.add(new ArrayList<>(comb));
+            return;
+        } else if (remain < 0) {
+            return;
         }
     }
 }
