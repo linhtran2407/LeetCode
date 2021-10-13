bca2aa5f20888e1de921a2c225886cc290335fd7
linhtran <ltran1@brynmawr.edu>
2021-10-13 18:02:44 -0400

    new

---
+++ Medium/src/ReverseLinkedList.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class ReverseLinkedList {
+    public static LinkedList reverseLinkedList(LinkedList head) {
+        if (head == null){
+                return null;
+            }
+            
+            LinkedList prev = null;
+            LinkedList cur = head;
+            while (cur != null){
+                LinkedList nextNode = cur.next;
+                cur.next = prev;
+                prev = cur;
+                cur = nextNode;
+            }
+        return prev;
+      }
+    
+      static class LinkedList {
+        int value;
+        LinkedList next = null;
+    
+        public LinkedList(int value) {
+          this.value = value;
+        }
+      }
+    }
+}
