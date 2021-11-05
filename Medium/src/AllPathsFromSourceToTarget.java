d611a3f6c190f86a4404bec14fa11b8a68f6e66b
linhtran <ltran1@brynmawr.edu>
2021-11-05 14:59:27 -0400

    backtrack

---
+++ Medium/src/AllPathsFromSourceToTarget.java
@@ -0,0 +1,29 @@
+public class AllPathsFromSourceToTarget {
+
+        public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+            List<List<Integer>> allPaths = new ArrayList<>();
+            if (graph[0].length == 0){
+                return allPaths;
+            }
+            
+            LinkedList<Integer> path = new LinkedList<>();
+            path.offer(0);
+            backtrack (allPaths, path, graph, 0, graph.length-1);
+            return allPaths;
+        }
+        
+        private void backtrack(List<List<Integer>> allPaths, LinkedList<Integer> path, int[][] graph, int currNode, int target){
+            if (currNode == target){
+                allPaths.add(new ArrayList<Integer>(path));
+                return;
+            }
+            
+            for (int neighbor: graph[currNode]){
+                path.add(neighbor);
+                backtrack(allPaths, path, graph, neighbor, target);
+                
+                path.removeLast();
+            }
+        }
+    
+}
