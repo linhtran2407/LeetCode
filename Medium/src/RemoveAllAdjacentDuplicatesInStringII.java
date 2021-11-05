01b8fcd8521fbde011b1a42028017a10f063877a
linhtran <ltran1@brynmawr.edu>
2021-11-04 21:48:04 -0400

    one more

--- Medium/src/RemoveAllAdjacentDuplicatesInStringII.java
+++ Medium/src/RemoveAllAdjacentDuplicatesInStringII.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class RemoveAllAdjacentDuplicatesInStringII {
     
 //     Runtime: 25 ms, faster than 60.84% of Java online submissions for Remove All Adjacent Duplicates in String II.
@@ -14,6 +16,28 @@ public class RemoveAllAdjacentDuplicatesInStringII {
                     // same chars
                     counts[i] = counts[i-1] + 1;
                     if (counts[i] == k){
+                        sb.delete(i-k+1, i+1);
+                        i = i - k;
+                    }
+                }
+            }
+            
+            return sb.toString();
+        }
+
+        public String removeDuplicates_stack(String s, int k) {
+            StringBuilder sb = new StringBuilder(s);
+            Stack<Integer> stack = new Stack<>();
+            for (int i = 0; i < sb.length(); i++){
+                // first char or not same as the prev char
+                if (stack.isEmpty() || sb.charAt(i) != sb.charAt(i-1)){
+                    stack.push(1);
+                } else {
+                    // same chars
+                    int top = stack.pop();
+                    stack.push(top+1);
+                    if (stack.peek() == k){
+                        stack.pop();
                         sb.delete(i-k+1, i+1);
                         i = i - k;
                     }
