a53b905c221181ed901d9614a26d8f85e0b9a9f4
linhtran <ltran1@brynmawr.edu>
2021-11-09 19:39:11 -0500

    kill process

---
+++ Medium/src/KillProcess.java
@@ -0,0 +1,31 @@
+public class KillProcess {
+
+    public List<Integer> killProcess(List<Integer> pid, List<Integer> ppid, int kill) {        
+        // build the graph: for each node, put a list of children
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for (int i = 0 ; i < ppid.size(); i++){
+            int val = ppid.get(i);
+            map.putIfAbsent(val, new ArrayList<>());
+            map.get(val).add(pid.get(i));
+        }
+
+        // do a dfs tree traversal from the killed node
+        List<Integer> killedNodes = new ArrayList<>();
+        killedNodes.add(kill);
+        findKilledNodes(killedNodes, kill, map);
+        return killedNodes;
+    }
+    
+    private void findKilledNodes(List<Integer> killedNodes, int kill, Map<Integer, List<Integer>> map){
+        // base
+        if (!map.containsKey(kill)){
+            return;
+        }
+        
+        for (int child: map.get(kill)){
+            killedNodes.add(child);
+            findKilledNodes(killedNodes, child, map);
+        }
+    }
+    
+}
