7db1fa5d27591cbf3d6c37acdc596cb5ef1c68ba
linhtran <ltran1@brynmawr.edu>
2021-11-09 19:50:00 -0500

    kill process

--- Medium/src/KillProcess.java
+++ Medium/src/KillProcess.java
@@ -1,6 +1,10 @@
+import java.util.*;
+
 public class KillProcess {
+
     // Runtime: 23 ms, faster than 70.17% of Java online submissions for Kill Process.
     // Memory Usage: 47.7 MB, less than 69.14% of Java online submissions for Kill Process.
+
     public List<Integer> killProcess(List<Integer> pid, List<Integer> ppid, int kill) {        
         // build the graph: for each node, put a list of children
         Map<Integer, List<Integer>> map = new HashMap<>();
