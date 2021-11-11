4bf819abae53936ae434d38607bb4a015c9f482f
linhtran <ltran1@brynmawr.edu>
2021-11-10 19:18:02 -0500

    reverse LL 2 ways

--- Medium/src/LinkedListCycleII.java
+++ Medium/src/LinkedListCycleII.java
@@ -18,6 +18,14 @@ public class LinkedListCycleII {
      * Definition for singly-linked list. class ListNode { int val; ListNode next;
      * ListNode(int x) { val = x; next = null; } }
      */
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     }
+     
     public ListNode detectCycle(ListNode head) {
         if (head == null || head.next == null) {
             return null;
