2df6442208c06927aca12807da92c2683b808cf0
linhtran <ltran1@brynmawr.edu>
2021-11-11 16:20:21 -0500

    top k freq words

---
+++ Medium/src/WordBreak.java
@@ -0,0 +1,37 @@
+public class WordBreak {
+    
+        public boolean wordBreak(String s, List<String> wordDict) {
+            // dp
+            // suppose opt[k] is the substring from start to k (k < len-1) that we know is segmentable
+            // return true if the substring [k+1, len) is contained in the word dictionary
+            // recurrence:
+            // opt[j] defined as true if the string (1, j) can be segmented
+            // opt[j] = 
+                // true, if (1, j) can be segmented
+                // true, if opt[k] = true and (k+1, j) can be segmented for all 1 <= k < len
+                // false, otherwise
+            
+            // "" l e e t c o d e
+            // 1        index = 3
+            
+            // build word dict
+            Set<String> dict = new HashSet<>();
+            dict.addAll(wordDict);
+            
+            int len = s.length();
+            boolean[] opt = new boolean[len+1];
+            opt[0] = true; // base case: empty string can be segmented
+            
+            for (int index = 0; index < len; index++){
+                for (int k = 0; k <= index; k++){
+                    if (opt[k] && dict.contains(s.substring(k, index+1))){
+                        opt[index+1] = true;
+                    }
+                }
+            }
+            
+            
+            return opt[len];
+        }
+    
+}
