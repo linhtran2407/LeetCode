0f454359820929a926ed58d0309111139f9e5bc7
linhtran <ltran1@brynmawr.edu>
2021-11-11 15:46:06 -0500

    top k freq words

---
+++ Medium/src/TopKFrequentWords.java
@@ -0,0 +1,32 @@
+public class TopKFrequentWords {
+    
+        public List<String> topKFrequent(String[] words, int k) {
+            // build freq map
+            Map<String, Integer> map = new HashMap<>();
+            Arrays.asList(words).forEach(word -> map.put(word, map.getOrDefault(word, 0) +1));
+            
+            // define min heap
+            // 1. store freq in ascending order
+            // 2. alphabetical in reverse order
+            // so that least prioritized word is always on the top
+            PriorityQueue<String> minHeap = new PriorityQueue<>((a,b) -> map.get(a) == map.get(b)? b.compareTo(a) : map.get(a) - map.get(b));
+            
+            
+            for (String word: map.keySet()){
+                minHeap.add(word);
+                if (minHeap.size() > k){
+                    minHeap.poll();
+                }
+            }
+            
+            // get result
+            // add in reverse order since this is min heap
+            List<String> res = new ArrayList<>();
+            while (!minHeap.isEmpty()){
+                res.add(0, minHeap.poll());
+            }
+            
+            return res;
+        }
+            
+}
