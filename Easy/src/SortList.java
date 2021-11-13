d6477197e55a5ba6337c9b9c3d9a28c71b357d46
linhtran <ltran1@brynmawr.edu>
2021-11-13 17:40:45 -0500

    sort list

---
+++ Easy/src/SortList.java
@@ -0,0 +1,59 @@
+public class SortList {
+    public class ListNode {
+        int val;
+        ListNode next;
+        public ListNode (int val, ListNode next){
+            this.val = val;
+            this.next = next;
+        }
+
+        public ListNode (){}
+        public ListNode (int val){
+            this.val = val;
+        }
+    }
+
+    public ListNode sortList(ListNode head){
+        if (head == null || head.next == null){
+            return head;
+        }
+
+        ListNode mid = getMid(head);
+        ListNode first = sortList(head);
+        ListNode second = sortList(mid);
+        return merge(first, second);
+    }
+
+    private ListNode getMid(ListNode head){
+        ListNode prevMid = null;
+        while (head != null && head.next != null){
+            prevMid = (prevMid == null)? head : prevMid.next;
+            head = head.next.next;
+        }
+
+        ListNode mid = prevMid.next;
+        prevMid.next = null;
+        return mid;
+    }
+
+    private ListNode merge (ListNode left, ListNode right){
+        ListNode dummy = new ListNode();
+        ListNode curr = dummy;
+
+        while (left != null && right != null){
+            if (left.val < right.val){
+                curr.next = new ListNode(left.val);
+                left = left.next;
+            } else {
+                curr.next = new ListNode(right.val);
+                right = right.next;
+            }
+            curr = curr.next;
+        }
+
+        curr.next = (left == null)? right : left;
+
+        return dummy.next;
+
+    }
+}
