6708a8dadddd8aa8560024afb54f82545b073a00
linhtran <ltran1@brynmawr.edu>
2021-11-14 13:58:11 -0500

    daily temp

--- Medium/src/DailyTemperatures.java
+++ Medium/src/DailyTemperatures.java
@@ -1,8 +1,24 @@
 import java.util.*;
 
 public class DailyTemperatures {
+    public class Pair{
+        int val;
+        int key;
+        public Pair (int key, int val){
+            this.val = val;
+            this.key = key;
+        }
+
+        public int getKey(){
+            return this.key;
+        }
+        public int getValue(){
+            return this.val;
+        }
+
+    }
     public int[] dailyTemperatures(int[] temperatures) {
-        Stack<Pair<Integer, Integer>> stack = new Stack<>();
+        Stack<Pair> stack = new Stack<>();
         int[] res = new int[temperatures.length];
         for (int i = 0; i < temperatures.length; i++){
             while (!stack.isEmpty() && stack.peek().getValue() < temperatures[i]){
